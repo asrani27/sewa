55fe5397cd13c62df5c142539e8e278d52cea646
asranim <asrani.27@gmail.com>
2020-12-02 09:34:05 +0800

    fix

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="webthemez">
+    <title>Sewa Gedung Banjarmasin</title>
+    <!-- core CSS -->
+    <link href="/theme/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/theme/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/theme/css/animate.min.css" rel="stylesheet">
+    <link href="/theme/css/owl.carousel.css" rel="stylesheet">
+    <link href="/theme/css/owl.transitions.css" rel="stylesheet">
+    <link href="/theme/css/prettyPhoto.css" rel="stylesheet">
+    <link href="/theme/css/magnific-popup.css" rel="stylesheet">
+    <link href="/theme/css/gallery-1.css" rel="stylesheet">
+    <link href="/theme/css/styles.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="/theme/images/ico/favicon.ico">
+</head>
+
+<body id="home">
+
+    <header id="header">
+        <nav id="main-nav" class="navbar navbar-default navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <div class="logoSection"> <a class="navbar-brand" href="index.html">Sewa</a>
+                        <span class="caption">Gedung BJM</span>
+                    </div>
+                </div>
+
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="scroll active"><a href="/">Home</a></li>
+                        <li class="scroll"><a href="/layanan">Layanan</a></li>
+                        <li class="scroll"><a href="/tentang-kami">Tentang Kami</a></li>
+                        <li class="scroll"><a href="/galery">Gallery</a></li>
+                        <li class="scroll"><a href="/harga">Harga</a></li>
+                        <li class="scroll"><a href="/kontak">Kontak</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.container-->
+        </nav>
+        <!--/nav-->
+    </header>
+    <!--/header-->
+
+    <section id="main-slider">
+        <div class="owl-carousel">
+            <div class="item"
+                style="background-image: url(https://www.intiland.com/wp-content/uploads/2019/08/Spazio-Office-Image-700x350-2.png);">
+                <div class="slider-inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="carousel-caption">
+                                <div class="carousel-content">
+                                    <h2>Sewa Gedung Banjarmasin</h2>
+                                    <p>Terbaik Di Banjarmasin</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/.item-->
+            <div class="item"
+                style="background-image: url(https://www.slp.id/upload/images/thumb-en-rental-factory_3.jpg);">
+                <div class="slider-inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="carousel-caption">
+                                <div class="carousel-content">
+                                    <h2>Sewa Gedung Banjarmasin</h2>
+                                    <p>Terbaik Di Banjarmasin</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <!--/.item-->
+        </div>
+        <!--/.owl-carousel-->
+    </section>
+    <!--/#main-slider-->
+
+    <section id="hero-text" class="wow fadeIn">
+        <div class="container">`
+            <div class="row">
+                <div class="col-sm-9">
+                    <h2>Sewa Gedung  Terbaik Di Banjarmasin</h2>
+                    <p>Berlokasi Di Jalan Ahmad Yani km 3, digunakan untuk acara wisuda,pernikahan dan lain-lain.
+                    </p>
+                </div>
+                <div class="col-sm-3 text-right">
+                    <a class="btn btn-primary btn-lg" href="#pricing">Harga!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/#hero-text-->
+
+    <section id="pricing">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">Harga Terbaik</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 col-md-6">
+                    <div class="wow zoomIn" data-wow-duration="400ms" data-wow-delay="0ms">
+                        <ul class="pricing">
+                            <li class="plan-header">
+                                <div class="price-duration">
+                                    <span class="price">
+                                        Gedung Raya 1
+                                    </span>
+                                </div>
+
+                                <div class="plan-name">
+                                    Fasilitas :
+                                </div>
+                            </li>
+                            <li><h4 class="media-heading">Parkir Luas</h4></li>
+                            <li><h4 class="media-heading">100 Buah Kursi</h4></li>
+                            <br>
+                            <li class="plan-purchase"><a class="btn btn-primary" href="#">Pesan Sekarang!</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                    <div class="wow zoomIn" data-wow-duration="400ms" data-wow-delay="200ms">
+                        <ul class="pricing featured">
+                            <li class="plan-header">
+                                <div class="price-duration">
+                                    <span class="price">
+                                        Gedung Raya 2
+                                    </span>
+                                    <span class="duration">
+                                        per hari
+                                    </span>
+                                </div>
+
+                                <div class="plan-name">
+                                    Promo
+                                </div>
+                            </li>
+                            <li>
+                                <p>Nique porriqua tquises dolorem
+                                    desumquia doloresamet consectet adipisci.
+                                    Masagni dolores eoquie voluptate sequi
+                                    saliquam quaerat voluptate.</p>
+                            </li>
+                            <li class="plan-purchase"><a class="btn btn-primary" href="#">Pesan Sekarang!</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--/#pricing-->
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2020 Asran Dev
+                </div>
+                <div class="col-sm-6">
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+
+    <script src="/theme/js/jquery.js"></script>
+    <script src="/theme/js/bootstrap.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="/theme/js/owl.carousel.min.js"></script>
+    <script src="/theme/js/mousescroll.js"></script>
+    <script src="/theme/js/smoothscroll.js"></script>
+    <script src="/theme/js/jquery.prettyPhoto.js"></script>
+    <script src="/theme/js/jquery.inview.min.js"></script>
+    <script src="/theme/js/wow.min.js"></script>
+    <script src="/theme/contact/jqBootstrapValidation.js"></script>
+    <script src="/theme/contact/contact_me.js"></script>
+
+    <script type="/theme/text/javascript" src="js/jquery.isotope.min.js"></script><!-- Gallery Filter -->
+    <script type="/theme/text/javascript" src="js/jquery.magnific-popup.min.js"></script><!-- Gallery Popup -->
+
+    <script src="/theme/js/custom-scripts.js"></script>
+</body>
+
+</html>
